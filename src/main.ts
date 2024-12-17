02d8060683a90b1f3d05000fbc0a434a125b384f
DanYunak <danyloyunak2006@gmail.com>
2024-12-17 22:21:58 +0200

    Changed cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const clientUrl = process.env.CLIENT_URL
+  // const clientUrl = process.env.CLIENT_URL
   
   app.enableCors({
-    origin: clientUrl,
+    origin: 'https://shop-frontend-r4w6.onrender.com',
     credentials: true
   })
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();

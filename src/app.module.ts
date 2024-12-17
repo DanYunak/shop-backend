8a979f5b2e7f83487c7d3f7f38983985387e1408
DanYunak <danyloyunak2006@gmail.com>
2024-12-17 21:38:05 +0200

    Finished version

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,15 +12,35 @@ import { configValidationSchema } from './config.schema';
       validationSchema: configValidationSchema,
       isGlobal: true
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true, // disable in production
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   autoLoadEntities: true,
+    //   synchronize: true, // disable in production
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          ssl: true,
+          extra: {
+            ssl: { rejectUnauthorized: false }
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE')
+        }
+      }
     }),
     ProductModule
   ],
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const clientUrl = process.env.CLIENT_URL
+  
+  app.enableCors({
+    origin: clientUrl,
+    credentials: true
+  })
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
